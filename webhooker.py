f5480e1ed940aa5507c7cba12f102e5133e58f00
Andrew Willaim Francis <102910297+yhun5589@users.noreply.github.com>
2025-10-15 15:45:54 +0700

    Update webhooker.py

--- webhooker.py
+++ webhooker.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
-from message_sender_line import send_msg
-import os
+from linebot.v3.messaging import MessagingApi, Configuration, ApiClient, PushMessageRequest, TextMessage
 
+CHANNEL_ACCESS_TOKEN = '5N9weT1wrJZK0InDEIw+yieWE+CT89KfwGHKarVkCBQ9Kd602FSPtS57rh5YZ4HxU66d9l9MsmRstoAEVs4SFBEBjWFR0+2fkQzXoSBlQPvK7rAtUF425p2wAMntUT0i7mKHaoqAhZ2R2QnTZvdVmQdB04t89/1O/w1cDnyilFU='
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 app = Flask(__name__)
+serverlink = ""
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -20,17 +22,36 @@ def callback():
         if msg.get("text") == "FIRST_TIME_SETTING":
             user_id = event.get("source", {}).get("userId")
             if user_id:
-                send_msg(user_id)
+                send_msg("your user_id:" + user_id, user_id)
+        elif msg.get("text") == "SETTING":
+            print("""
+                    type: FIRST_TIME_SETTING to view your user id
+                    type: LINK to get the server link
+                  """
+                  )
+        elif msg.get("text") == "LINK":
+            send_msg(serverlink, user_id)
+
+        elif msg.get("text") == "HELP":
+            web_link = ""
+            send_msg(web_link, user_id)
+        else:
+            send_msg("Unknown command. Please type 'SETTING' for options.", user_id)
 
     return "OK", 200
 
+def send_msg(texttosend, USER_ID):
+    with ApiClient(configuration) as api_client:
+        messaging_api = MessagingApi(api_client)
 
-# Optional test route to prevent 404 on root
-@app.route("/", methods=['GET'])
-def home():
-    return "Bot is running!", 200
+        message = TextMessage(text=texttosend)
 
+        request = PushMessageRequest(
+            to=USER_ID,
+            messages=[message]
+        )
+
+        messaging_api.push_message(request)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
