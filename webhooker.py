e95351d04c15c7e094ee4a0a7f9a9e11e93e455d
Andrew Willaim Francis <102910297+yhun5589@users.noreply.github.com>
2025-10-15 16:15:13 +0700

    Update webhooker.py

--- webhooker.py
+++ webhooker.py
@@ -54,4 +54,5 @@ def send_msg(texttosend, USER_ID):
         messaging_api.push_message(request)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
